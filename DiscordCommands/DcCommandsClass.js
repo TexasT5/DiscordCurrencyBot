174f7269e822c143e7250ec4a23e48118f2a396f
TexasT5 <mertkrgl61@gmail.com>
2022-04-14 04:42:58 +0300

    master

--- DiscordCommands/DcCommandsClass.js
+++ DiscordCommands/DcCommandsClass.js
@@ -3,6 +3,27 @@ class DcCommands{
         if(message == null || message == "") return "err";
         channel.channel.send(message)
     }
+
+    registerUser(username , message){
+        if(username == "" || username == null) return "err";
+        if(message == null) return 'err'
+
+        message.mentions.members.first().setNickname(`${username}`)
+          .then(data =>{
+            message.channel.send('Başarıyla değiştirildi aga bir kontrol et')
+            message.guild.roles.cache.find(r => {
+              if(r.id === "640155688081031182"){
+                message.mentions.members.first().roles.add(r)
+              }
+            });
+          })
+          .catch(e =>{
+            message.channel.send('Aga senin rütbenin üstündeki birinin ismini değiştiremezsin söyleyeyim. \nHa diyorsan ki amacım o değildi bir geliştirici ile konuşayım')
+          })
+
+    }
+
+
 }
 
 module.exports = DcCommands
--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { Client, Intents , Permissions } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ] });
+const permission = new Permissions('MANAGE_NICKNAMES')
 const config = require('./config.json')
 const discordCommands = require("./command.json")
-const commands = require("./TCommands/command.js")
+const commands = require("./TCommands/command.js");
+const discordCommandClass = require('./DiscordCommands/DcCommandsClass')
+const discordCommdans = new discordCommandClass()
+
+
+client.on('ready' , () => {
+  permission.add('MANAGE_NICKNAMES')
+})
+
 
 client.on("messageCreate", (message) => {
   if (!message.content.startsWith(config.prefix) || message.author.bot) return
@@ -10,6 +19,22 @@ client.on("messageCreate", (message) => {
       var sliceText = message.content.split(`${config.prefix}${discordCommands.kur} `)
       if( sliceText[1] == null || sliceText[1] == "") message.channel.send('Error ! enter a valid currency')
       else commands.whatUserWrite(sliceText[1].toUpperCase() , message)
+  }else if(message.content.startsWith(`${config.prefix}${discordCommands.kayit} `)){
+    if (message.member.roles.cache.some(role => role.name === "💻┊Yönetim") == true) {
+      if(message.guild.me.permissions.has('MANAGE_NICKNAMES') == false){
+        message.channel.send('İznim yok bilader 1 dakika müsade et izini vereyim')
+        message.guild.me.permissions.add('MANAGE_NICKNAMES')
+        message.channel.send('Tamam verilmis olmasi gerekiyor tekrar dene')
+      }else{
+        var splitUsername  = message.content.split(`<@${message.mentions.members.first().id}> `)
+        if(splitUsername[1] == null) return message.channel.send('Kullanıcı adı nere bilader kayıt etmiyorum s.g')
+        else{
+          if (discordCommdans.registerUser(splitUsername[1] , message) == 'err') return message.channel.send('Aga ufak bir hata oldu tekrar denesene')          
+        }
+      }  
+    }else{
+       message.channel.send('sen sg')
+    }
   }
 });
 
